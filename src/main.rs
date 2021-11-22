cef8e3b86e74fb0d669f5848d225219d13537104
Lunarmagpie <Bambolambo0@gmail.com>
2021-11-21 22:47:27 -0500

    error handling

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,33 @@ use std::env::args;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+use std::process::exit;
 
 fn main() {
-    let filename = &args().collect::<Vec<String>>()[1];
-    // Create a path to the desired file
-    let path = Path::new(filename);
+    let args = &args().collect::<Vec<String>>();
+    if args.len() <= 1 {
+        println!("No file inputted.");
+        exit(1);
+    }
+
+    let path = Path::new(&args[1]);
     let display = path.display();
 
-    // Open the path in read-only mode, returns `io::Result<File>`
     let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Err(why) => {
+            println!("couldn't open {}: {}", display, why);
+            exit(1);
+        }
         Ok(file) => file,
     };
 
     // Read the file contents into a string, returns `io::Result<usize>`
     let mut s = String::new();
     match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Err(why) => {
+            println!("couldn't read {}: {}", display, why);
+            exit(1);   
+        },
         Ok(_) => interpret(s),
     }
 }
